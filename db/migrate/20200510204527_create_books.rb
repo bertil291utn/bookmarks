ce069222caf2a40849856576b67471ad0ae8eace
bertil291utn <bertilt4@gmail.com>
2020-05-10 19:26:22 -0500

    Fix  table data type - Update shcema

--- db/migrate/20200510204527_create_books.rb
+++ db/migrate/20200510204527_create_books.rb
@@ -2,12 +2,12 @@ class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
       t.string :Review
-        t.string :title
-        t.string :author
-        t.string :description
-        t.string :publisher
-        t.int :weeks_on_list
-        t.int :rank_this_week
+      t.string :title
+      t.string :author
+      t.string :description
+      t.string :publisher
+      t.integer :weeks_on_list
+      t.integer :rank_this_week
       t.timestamps
     end
   end
